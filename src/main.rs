b3790762b5013b2041fdb9dbbe99eea528a6da9e
filippo <filippotobias@gmail.com>
2024-06-12 20:42:38 +0100

    ? commit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 use bevy::prelude::*;
 use bevy_editor_pls::*;
 
+mod card_container;
+mod mini_editor;
+
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(EditorPlugin::default())
-    .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
-    .add_plugins(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+    .add_plugins((DefaultPlugins,
+        EditorPlugin::default(),
+        bevy::diagnostic::FrameTimeDiagnosticsPlugin,
+        bevy::diagnostic::EntityCountDiagnosticsPlugin,
+        card_container::CardContainer,
+        mini_editor::MiniEditor{editor_open: false}
+    
+    ))
+    .add_systems(Startup, setup_cam)
     .run()
+}
+
+fn setup_cam(
+    mut commands: Commands,
+) {
+    let camera = Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    };
+    commands.spawn(camera);
 }
---
+++ src/mini_editor.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+#[derive(Component)]
+pub struct MiniEditor{
+    pub editor_open: bool,
+}
+
+impl Plugin for MiniEditor{
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, initialize_editor);
+        app.add_systems(Update, launch_editor);
+    }
+}
+
+fn initialize_editor(
+    mut commands: Commands
+) {
+    commands.spawn(MiniEditor{editor_open: false});
+}
+
+fn launch_editor(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut MiniEditor>,
+) {
+    if keys.just_pressed(KeyCode::KeyA){
+        for mut editor in query.iter_mut() {
+            editor.editor_open = true;
+            println!("{}", editor.editor_open.to_string());
+        }
+    }
+}
