2c756e3dab7281441168c4bb74a69b6140d12fda
filippo <filippotobias@gmail.com>
2024-06-18 17:01:08 +0100

    camera_fixed

--- src/game_camera.rs
+++ src/game_camera.rs
@@ -42,7 +42,7 @@ impl Plugin for GameCamera {
 fn setup(mut commands: Commands) {
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 6.0, 12.0).looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
+            transform: Transform::from_xyz(0.0, 6.0, 6.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
             camera: Camera {
                 target: RenderTarget::Window(WindowRef::Primary),
                 ..default()
@@ -63,7 +63,7 @@ fn pan_camera(
         for event in mouse_event_reader.read() {
             let delta = event.delta;
             transform.translation.x -= delta.x * 0.01;
-            transform.translation.y += delta.y * 0.01;
+            transform.translation.z -= delta.y * 0.01;
         }
     }
 }
