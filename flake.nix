63130836c8b35d1c4c12c257c04c8453d102f1c9
filippo <filippotobias@gmail.com>
2024-07-25 19:04:57 +0100

    alot

--- flake.nix
+++ flake.nix
@@ -16,11 +16,16 @@
                     pkg-config
                 ];
                 buildInputs = with pkgs; [
-                    cargo rustc
+                    rustup
+                    rust-analyzer
                     udev alsa-lib vulkan-loader
                     xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
                     libxkbcommon wayland # To use the wayland feature
                 ];
+                shellHook = ''
+                  rustup default stable
+                  rustup component add rust-analyzer
+                '';
                 LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
                 RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             };
