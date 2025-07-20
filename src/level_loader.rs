3a419771edf8ae8f154057f9dd6261908524ae2f
filippo <filippotobias@gmail.com>
2025-07-20 13:28:54 +0100

    reworking level loader to have function to create different areas of tiles

--- src/level_loader.rs
+++ src/level_loader.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 use bevy::{math::DVec3, prelude::*, utils::hashbrown::HashMap};
 use crate::floor_tile_mesh;
 
@@ -14,7 +16,7 @@ pub struct LevelLoaderPlugin;
 impl Plugin for LevelLoaderPlugin {
     fn build(&self, app: &mut App) {
         app.
-        add_systems(Startup, (load_map, make_map))
+        add_systems(Startup, make_default_map)
         .insert_resource(Map {
             tower_positions: HashMap::new(),
         })
@@ -22,8 +24,20 @@ impl Plugin for LevelLoaderPlugin {
     }
 }
 
-fn load_map(
+fn make_default_map (
+    commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let map_ranges = vec![(DVec3{x: -18.0,y: 0.0,z: -22.0}, DVec3{x: 18.0,y: 0.0,z: 7.0})];
+    make_map(commands, meshes, materials, map_ranges);
+}
+
+fn make_map(
     mut commands: Commands,
+    meshes: ResMut<Assets<Mesh>>,
+    materials: ResMut<Assets<StandardMaterial>>,
+    map_ranges: Vec<(DVec3,DVec3)>
 ) {
     // light
     commands.spawn((
@@ -35,40 +49,40 @@ fn load_map(
         },
         Transform::from_xyz(8.0, 16.0, 8.0),
     ));
+    build_map(commands, meshes, materials, map_ranges);
 }
 
-fn make_map(
+fn build_map(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    map_ranges: Vec<(DVec3,DVec3)>
 ) {
-    let spawn_positions = (DVec3{x: -21.6,y: 0.0,z: -26.4}, DVec3{x: 21.6,y: 0.0,z: 8.4});
-    //let spawn_positions = (DVec3{x: -3.6,y: 0.0,z: -3.6}, DVec3{x: 3.6,y: 0.0,z: 3.6});
-    //let spawn_positions = (DVec3{x: 0.0,y: 0.0,z: 0.0}, DVec3{x: 1.2,y: 0.0,z: 1.2});
     let floor_tile_mesh = floor_tile_mesh::create_floor_tile_mesh(&mut meshes);
-
-    //Calculate the amount of tiles
-    let tiles_x = ((spawn_positions.0.x - spawn_positions.1.x).abs() / 1.2).round() as i32;
-    let tiles_z = ((spawn_positions.0.z - spawn_positions.1.z).abs() / 1.2).round() as i32;
-    let mut position: DVec3 = spawn_positions.0;
-    let material_handle = materials.add(StandardMaterial {
-        base_color: Color::srgb(0.2, 0.2, 0.2),
-        ..Default::default()
-    });
-    for _i in 0..tiles_x {
-        for _j in 0..tiles_z{
-            commands.spawn((
-                Mesh3d(floor_tile_mesh.clone()),
-                MeshMaterial3d(material_handle.clone()),
-                Transform::from_xyz(position.x as f32, position.y as f32, position.z as f32),
-            ))
-            .observe(on_map_drag)
-            ; //Find way to declare bundle once and clone it inside the loop.
-            //println!("x: {}, y: {}, z: {}, i: {}, j: {}, tiles_x: {}", position.x, position.y, position.z, i, j, tiles_x);
-            position.z += 1.2;
+    for range in map_ranges {
+        let spawn_positions = (DVec3{x: range.0.x * 1.2,y: range.0.y,z: range.0.z * 1.2}, DVec3{x: range.1.x * 1.2,y: range.1.y,z: range.1.z * 1.2});
+        //Calculate the amount of tiles
+        let tiles_x = ((spawn_positions.0.x - spawn_positions.1.x).abs() / 1.2).round() as i32;
+        let tiles_z = ((spawn_positions.0.z - spawn_positions.1.z).abs() / 1.2).round() as i32;
+        let mut position: DVec3 = spawn_positions.0;
+        let material_handle = materials.add(StandardMaterial {
+            base_color: Color::srgb(0.2, 0.2, 0.2),
+            ..Default::default()
+        });
+        for _i in 0..tiles_x {
+            for _j in 0..tiles_z{
+                commands.spawn((
+                    Mesh3d(floor_tile_mesh.clone()),
+                    MeshMaterial3d(material_handle.clone()),
+                    Transform::from_xyz(position.x as f32, position.y as f32, position.z as f32),
+                ))
+                .observe(on_map_drag);
+                //println!("x: {}, y: {}, z: {}, i: {}, j: {}, tiles_x: {}", position.x, position.y, position.z, i, j, tiles_x);
+                position.z += 1.2;
+            }
+            position.z = spawn_positions.0.z;
+            position.x += 1.2;
         }
-        position.z = spawn_positions.0.z;
-        position.x += 1.2;
     }
 }
 
